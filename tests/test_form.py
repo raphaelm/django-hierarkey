6b2822f544625176b826175497488c17cf0dca26
Raphael Michel <michel@rami.io>
2025-06-10 13:02:54 +0200

    Fix tests

--- tests/test_form.py
+++ tests/test_form.py
@@ -18,7 +18,9 @@ class SampleForm(HierarkeyForm):
 
 @pytest.fixture
 def organization():
-    return Organization.objects.create(name='Foo')
+    o = Organization.objects.create(name='Foo')
+    o.settings.flush()
+    return o
 
 
 @pytest.mark.django_db
@@ -51,7 +53,16 @@ def test_form_save_plain_values(organization):
 
 @pytest.mark.django_db
 def test_form_ignore_newline_change(organization):
-    organization.settings.text_string == 'foo\nbar'
+    form = SampleForm(obj=organization, attribute_name='settings', data={
+        'test_string': 'foo\nbar',
+        'test_int': 42
+    })
+    assert form.is_valid()
+    form.save()
+    organization.settings.flush()
+    assert organization.settings.test_string == 'foo\nbar'
+    assert organization.settings.test_int == '42'
+
     form = SampleForm(obj=organization, attribute_name='settings', data={
         'test_string': 'foo\r\nbar',
         'test_int': 42
